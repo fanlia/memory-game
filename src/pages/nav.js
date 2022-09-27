c7bd986dafd3bda85f15d4c9a7aa86d77333e51a
fanlia <you@example.com>
2022-09-27 13:11:21 +0800

    first commit

--- src/pages/nav.js
+++ src/pages/nav.js
@@ -1,7 +1,7 @@
 
 export default `
 <p>
-    <a href='#/' component='a' data-active-route-class='route-active'>游戏</a>
-    <a href='#/about' component='a' data-active-route-class='route-active'>记忆</a>
+    <a href='/' component='a' data-active-route-class='route-active'>游戏</a>
+    <a href='/about' component='a' data-active-route-class='route-active'>记忆</a>
 </p>
 `
